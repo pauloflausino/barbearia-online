6994d5bce653c03f8801e3957ecd871a1c5bed16
Paulo Ferreira <pauloflausino@gmail.com>
2026-06-05 14:43:34 -0300

    atualizando rotas

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,10 +11,19 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->alias([
-            'tenant' => \App\Http\Middleware\TenantMiddleware::class,
-        ]);
+        $middleware->redirectGuestsTo(function ($request) {
+        // Tenta capturar o slug tanto se vier como parâmetro de rota ou pela URL (/b/slug/...)
+        $tenantSlug = $request->route('tenant_slug') ?? $request->segment(2);
+
+        if ($tenantSlug) {
+            return route('tenant.login', ['tenant_slug' => $tenantSlug]);
+        }
+
+        return '/'; // Fallback caso aceda a algo fora de um tenant
+        });
     })
+
+       
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
